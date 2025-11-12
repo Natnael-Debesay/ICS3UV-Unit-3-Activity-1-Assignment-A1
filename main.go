40e220b3bef2ab59c4969d27eed414d753763c0d
default <natnael.debesay@stu.ocsb.ca>
2025-11-12 01:21:57 +0000

    added main.go file

---
+++ main.go
@@ -0,0 +1,28 @@
+/* 
+ * Author Natnael Debesay
+ * Version 1.0.0
+ * Date 2025-11-11
+ * This program finds the area of a circle.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	// initialize numbers as constants
+	const RADIUS float64 = 5.6
+	const PI float64 = 3.14
+
+	// INPUT - none
+
+	// PROCESS 
+	// calulate the sum
+	answer := RADIUS * RADIUS * PI
+
+	// OUTPUT
+	// display the result
+	fmt.Println("The area of a circle with a radius of " , 5.6, "is:", answer,)
+
+	fmt.Println("\nDone.")
+}
---
+++ main.ts
@@ -0,0 +1,25 @@
+/**
+ * @author Natnael Debesay
+ * @version 1.0.0 
+ * @date 2025-11-11
+ * @fileoverview This program finds the area of a circle.
+ */
+
+// initialize numbers
+const RADIUS: number = 5.6;
+const PI: number = 3.14;
+
+// INPUT - none
+
+// PROCESS
+// calculate the area
+
+let answer: number = RADIUS * RADIUS * PI;
+
+// OUTPUT
+// display the result 
+console.log(
+  "The area of a circle with a radius of " + RADIUS + " is: " + answer
+);
+
+console.log("\nDone.");
